94d14f99075a41e68099846ece870f7b8b7fa816
Nisha  V <87647027+nisha250320@users.noreply.github.com>
2022-04-03 16:18:33 +0530

    Update percentage.c

--- implementation/src/percentage.c
+++ implementation/src/percentage.c
@@ -1,1 +1,17 @@
 
+#include "conversion.h"
+/**
+ * @brief This function caluculate the pecentage
+ * 
+ * @param ch 
+ * @param num1 
+ * @return float 
+ */
+float percentage(int ch,int num1)
+{
+    float percent=0;
+    percent = (float)num1 / ch * 100.0;
+    printf("Percentage is %0.3f\n\n",percent);
+    printf("choose next operation:\n");
+    return percent;
+}
